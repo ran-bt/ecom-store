e9b41ef07c8b23ae9ec8ebb45361890b05afe1b5
Inran <inranb@gmail.com>
2023-04-27 18:45:25 -0600

    set up link routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import Categories from "./components/categories/Categories";
+// import Categories from "./components/categories/Categories";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/home/Home";
+import Navigation from "./routes/navigation/Navigation";
+import SignIn from "./routes/sign-in/SignIn";
 
 const App = () => {
-  return <Categories />;
+  const Shop = () => {
+    return <h1> Welcome to to shop</h1>;
+  };
+
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index={true} element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/routes/sign-in/SignIn.jsx
@@ -0,0 +1,5 @@
+const SignIn = () => {
+  return <div>Welcome to the sign in Page!</div>;
+};
+
+export default SignIn;
